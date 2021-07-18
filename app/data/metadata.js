61c67cf1245fba4dd6786607e76abfe6ffb249b1
jason-choi-skku <jasonchoi3@g.skku.edu>
2021-07-19 00:09:50 +0900

    INIT

---
+++ app/data/metadata.js
@@ -0,0 +1,37 @@
+export const metaData = [{
+        id : 0,
+        name : "INPUT",
+        x : 100, 
+        y : 50,
+    },{
+        id : 1,
+        name : "1X1 CONV",
+        x : 200, 
+        y : 50,
+    },{
+        id : 2,
+        name : "3X3 CONV",
+        x : 300, 
+        y : 50,
+    },{
+        id : 3,
+        name : "5X5 CONV",
+        x : 400, 
+        y : 50,
+    },{
+        id : 4,
+        name : "5X5 CONV",
+        x : 500, 
+        y : 50,
+    },{
+        id : 5,
+        name : "3X3 MAXPOOL",
+        x : 600, 
+        y : 50,
+    },{
+        id : 6,
+        name : "OUTPUT",
+        x : 700, 
+        y : 50,
+    }]
+    
